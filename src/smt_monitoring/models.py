b4a8c4c6d07d6e530a58029f44b72420bac2382c
Sebastian Olszak <sebastian.olszak@icloud.com>
2025-09-15 21:03:17 +0200

    models

--- src/smt_monitoring/models.py
+++ src/smt_monitoring/models.py
@@ -1,3 +1,29 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    timestamp = models.DateTimeField()
+    lane = models.SmallIntegerField()
+    mc = models.CharField(max_length=10)
+    fadd = models.IntegerField()
+    fsadd = models.SmallIntegerField()
+    pn = models.CharField(max_length=20)
+    feeder_serial = models.CharField(max_length=30, null=True, blank=True)
+    pickup = models.IntegerField(validators=[MinValueValidator(0)])
+    pmiss = models.IntegerField(validators=[MinValueValidator(0)])
+    rmiss = models.IntegerField(validators=[MinValueValidator(0)])
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["timestamp", "lane", "mc", "fadd", "fsadd", "pn"],
+                name="unique_event_key"
+            )
+        ]
+        indexes = [
+            models.Index(fields=["timestamp", "lane", "mc"]),
+            models.Index(fields=["pn"]),
+        ]
+
+    def __str__(self):
+        return f"{self.timestamp} |Lane: {self.lane} | Slot: {self.fadd}.{self.fsadd} | Part: {self.pn}"
