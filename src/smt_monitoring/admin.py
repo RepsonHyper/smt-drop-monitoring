21ca7089cf9f4a34713eb5de4639329c730c714d
Sebastian Olszak <sebastian.olszak@icloud.com>
2025-09-15 19:09:46 +0200

    chore: admin branding

--- src/smt_monitoring/admin.py
+++ src/smt_monitoring/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = "SMT Monitoring — Admin"
+admin.site.site_title = "SMT Monitoring"
+admin.site.index_title = "Dashboard administracyjny"
